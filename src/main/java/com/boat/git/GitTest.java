d496d8f7e99cf986ee46de728f670b143e645f59
OceanBoat <1508369858@qq.com>
2022-02-10 20:10:43 +0800

    second commit

--- src/main/java/com/boat/git/GitTest.java
+++ src/main/java/com/boat/git/GitTest.java
@@ -7,5 +7,6 @@ package com.boat.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello handsome");
+        System.out.println("hello handsome");
     }
 }
